5533848ab4ca572337c0d1904ba1665c0623afcf
orzel320 <52106515+orzel320@users.noreply.github.com>
2026-06-16 00:51:35 +0200

    Poprawiono typo w docs.yml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,26 +1,23 @@
 import os
 import sys
 
-# 1. Wskazujemy ścieżkę do backendu (aby autodoc znalazł kod Pythona)
 sys.path.insert(0, os.path.abspath('../../backend'))
 
 project = 'Radioaktywność - Baza Grafowa'
-copyright = '2024, Twój Autor'
-author = 'Twój Autor'
+copyright = ''
+author = 'Rafał Oszajca'
 
 # 2. Aktywacja wymaganych rozszerzeń
 extensions = [
-    'sphinx.ext.autodoc',      # Czytanie docstringów z Pythona
-    'sphinx.ext.napoleon',     # Obsługa formatu Google w docstringach
-    'sphinx_js',               # Czytanie JSDoc z JS
-    'sphinxcontrib.mermaid',   # Obsługa diagramów Mermaid
-    'myst_parser',             # Obsługa plików Markdown (.md)
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_js',
+    'sphinxcontrib.mermaid',
+    'myst_parser',
 ]
 
-# 3. Wskazujemy ścieżkę do kodu frontendowego (dla sphinx-js)
 js_source_path = '../../frontend'
 
-# 4. Ustawienia plików źródłowych
 source_suffix = {
     '.rst': 'restructuredtext',
     '.md': 'markdown',
@@ -30,6 +27,5 @@ templates_path = ['_templates']
 exclude_patterns = []
 language = 'pl'
 
-# 5. Ustawienie nowoczesnego motywu Furo
 html_theme = 'furo'
 html_static_path = ['_static']
